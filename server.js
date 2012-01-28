a48a0c95c8d1c9bddc7f7c4f19d726fe9ef96b81
Gustavo Lacoste <gustavo@lacosox.org>
2012-01-28 10:52:53 -0300

    ejemplito

---
+++ server.js
@@ -0,0 +1,20 @@
+
+process.addListener('uncaughtException',function(err,stack){
+  console.log('------------------');
+  console.log('Exception: '+err);
+  console.log(err.stack);
+  console.log('------------------');
+});
+
+var LiveStats = require('./lib/livestats');
+new LiveStats({
+  port: 8000,
+  geoipServer:{
+    hostname: 'geoip.peepcode.com'
+   , port: 80
+  }
+});
+
+
+
+
---
+++ var.sh
@@ -0,0 +1,3 @@
+NODE_PATH=/home/knx/public_html/testnode/ejemplo/livestats/vendor
+export NODE_PATH
+node server.js
